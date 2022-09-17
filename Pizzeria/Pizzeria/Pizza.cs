311f3ca699db920e4043928952371a8ec724ecec
Winststreloc <68554095+Winststreloc@users.noreply.github.com>
2022-09-17 23:35:13 +0300

    Revert "Merge test develop with task7"

--- Pizzeria/Pizzeria/Pizza.cs
+++ Pizzeria/Pizzeria/Pizza.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace Pizzeria
 {
-    public class Pizza: Order
+    public class Pizza
     {
+        public int Id { get; set; }
 
+
+        public string Name { get; set; }
+        public PizzaSize Size { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public decimal Price { get; set; }
     }
 
     public enum PizzaSize { Small, Medium, Large }
