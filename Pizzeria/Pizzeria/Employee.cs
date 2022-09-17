0d8bbd5f914aca109e927e4c08a523ebfb446630
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-17 23:21:51 +0300

    task9

---
+++ Pizzeria/Pizzeria/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int WorkExperience { get; set; }
+        public void PrintName()
+        {
+            Console.WriteLine($"The employee's name is {Name}, he is {Age} years old, work experience is {WorkExperience}");
+        }
+
+    }
+}
