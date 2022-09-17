7310fde100baf4ab4e49f9a3d16aecec59e4a428
TgT60 <dima.icigo@yandex.ru>
2022-09-18 00:02:28 +0300

    add class Number

---
+++ Pizzeria/Pizzeria/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria
+{
+    class Number
+    {
+        public string CallNumber { get; set; }
+        public void Call()
+        {
+            Console.WriteLine($"+{CallNumber}");
+        } 
+    }
+}
