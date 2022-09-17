5de8dfd4692373d37841fc422bf4e5d3682bbd32
TgT60 <dima.icigo@yandex.ru>
2022-09-17 21:28:55 +0300

    Create  New class Pizzeria and Adress

---
+++ Pizzeria/Pizzeria/Adress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria
+{
+    class Adress
+    {
+        public string  CustumerAdress {get;set;}
+        public void Delivery()
+        {
+            Console.WriteLine($"Delivery order to {CustumerAdress}");
+        }
+
+
+    }
+}
