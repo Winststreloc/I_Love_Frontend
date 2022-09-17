b801cff5e869b2c82477c73339b8ef3cdb1c197f
winststreloc <goloologo@gmail.com>
2022-09-18 00:58:18 +0300

    update all

--- Pizzeria/Pizzeria/Order.cs
+++ Pizzeria/Pizzeria/Order.cs
@@ -6,17 +6,26 @@ namespace Pizzeria
 {
     public class Order
     {
-        private decimal Price { get; set; }   
+        private decimal Price { get; set; }
+
         private DateTime TimeCreateOrder { get; set; }
+
         public int Id { get; set; }
-        public string Name { get; set; }
-        public PizzaSize Size { get; set; }
-        public bool IsGlutenFree { get; set; }
+        public PizzaSize PizzaSize { get; }
+        public string ClientName { get; set; }
+        public PizzaName PizzaName { get;}
+        public bool NeedDelivery { get; set; }
 
 
-        public Order(int id, string name, PizzaSize size, bool gluten,  DateTime time, decimal price)
+        public Order(int id, string name, PizzaSize size, PizzaName pizzaName, bool deliver,  DateTime time, decimal price, string clientName)
         {
-
+            Id = id;
+            PizzaSize = size;
+            PizzaName = pizzaName;
+            NeedDelivery = deliver;
+            TimeCreateOrder = time;
+            Price = price;
+            ClientName = clientName;
         }
 
 
--- Pizzeria/Pizzeria/Pizza.cs
+++ Pizzeria/Pizzeria/Pizza.cs
@@ -6,8 +6,13 @@ namespace Pizzeria
 {
     public class Pizza: Order
     {
+        public Pizza()
+        {
+
+        }
 
     }
 
     public enum PizzaSize { Small, Medium, Large }
+    public enum PizzaName { Pepperoni, Margherita, Cheese }
 }
