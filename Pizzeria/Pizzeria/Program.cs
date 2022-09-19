2544dd19d068df39d4d7d7ddc7ef8f283e2c363b
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-20 00:47:25 +0300

    program

--- Pizzeria/Pizzeria/Program.cs
+++ Pizzeria/Pizzeria/Program.cs
@@ -10,9 +10,11 @@ namespace Pizzeria
             Adress clientLocation = new Adress("Belarus", "Minsk", "Yakuba Kolasa", "26");
             Client client = new Client("Ivan", clientLocation);
             Order order = client.CreateOrder(client.Name, PizzaName.Margherita, PizzaSize.Medium, true);
-            ShopAssistant assistant = new ShopAssistant("Ighar");
-            assistant.AcceptOrder(order);
+            var deliveryman = new Deliveryman { Client = client };
+            var shopAssistant = new ShopAssistant("Igor") { Deliveryman = deliveryman };
+            shopAssistant.AcceptOrder(order);
+            
 
-        } 
+        }
     }
 }
