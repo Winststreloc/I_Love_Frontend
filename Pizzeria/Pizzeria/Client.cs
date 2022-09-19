9f1beab9308c581597f4368e3a45d926aa4b7258
winststreloc <goloologo@gmail.com>
2022-09-19 17:12:04 +0300

    connect

--- Pizzeria/Pizzeria/Client.cs
+++ Pizzeria/Pizzeria/Client.cs
@@ -7,21 +7,28 @@ namespace Pizzeria
 {
     public class Client
     {
-        //Location location { get; } //TODO
+        private readonly Adress location;
+
+        public Adress GetLocation()
+        {
+            return location;
+        }
+
         List<Order> Orders { get; }
         public string Name { get; set; }
-        public Client(string name)
+        public Client(string name, Adress location)
         {
             Name = name;
+            this.location = location;
         }
         public Order Get(int id)
         {
             return Orders.FirstOrDefault(p => p.Id == id);
         }
-        public Order CreateOrder(string clientName, Pizza pizza, bool deliver)
+        public Order CreateOrder(string clientName, PizzaName pizzaName, PizzaSize pizzaSize, bool deliver)
         {
             Name = clientName;
-            Order order = new Order(pizza, deliver, clientName);
+            Order order = new Order(pizzaName, pizzaSize, deliver, clientName);
             return order;
         }
         public void DeleteOrder(int id)
--- Pizzeria/Pizzeria/Employee/Deliveryman.cs
+++ Pizzeria/Pizzeria/Employee/Deliveryman.cs
@@ -9,7 +9,7 @@ namespace Pizzeria
         public Client Client { get; set; }
         public void Deliver(Pizza pizza)
         {
-            Console.WriteLine($"order {pizza.PizzaName}send to {Client.Name} to adress {Client.Location}");
+            Console.WriteLine($"order {pizza.PizzaName}send to {Client.Name} to adress {Client.GetLocation()}");
         }
     }
 }
