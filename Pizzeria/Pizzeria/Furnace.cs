3f1a888033cc7ae78753d51c181388638240f1ea
winststreloc <goloologo@gmail.com>
2022-09-19 14:46:00 +0300

    merge Equipment with dev

---
+++ Pizzeria/Pizzeria/Furnace.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria
+{
+    class Furnace:Equipment
+    {
+        public readonly string _name;
+        public readonly int _inventory_number;
+        private string Firm { get; set; }
+        private int Power { get; set; }
+        private int Capacity { get; set; }
+        private double Cost { get; set; }
+        private const double _tariff = 0.2;
+
+        public Furnace(Equipment obj,string firm,int capacity)
+        {
+            _name = obj.name_of_equip;
+            _inventory_number = obj.inventory_number;
+            Firm = firm;
+        }
+        public double Electricity_costs(Furnace obj, double time)
+        {
+            double cost = obj.Power * time*_tariff;
+
+            return cost;
+        }
+    }
+}
---
+++ Pizzeria/Pizzeria/Other_equip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria
+{
+    class Other_equip:Equipment
+    {
+        private string Type { get; set;}
+        private decimal Value { get; set; }
+        public Other_equip(decimal value,string type)
+        {
+            Type = type;
+            Value = value;
+        }
+        
+    }
+}
