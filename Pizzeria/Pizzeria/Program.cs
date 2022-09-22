423fc7bd74d8cf7a703daa043b6e7ddd66206925
winststreloc <goloologo@gmail.com>
2022-09-22 16:05:40 +0300

    I add Datatarime in Equipment

--- Pizzeria/Pizzeria/Program.cs
+++ Pizzeria/Pizzeria/Program.cs
@@ -9,13 +9,10 @@ namespace Pizzeria
             Pizzeria pizzeria = new Pizzeria();
             Adress clientLocation = new Adress("Belarus", "Minsk", "Yakuba Kolasa", "26");
             Client client = new Client("Ivan", clientLocation);
-            Order order = client.CreateOrder(client.Name, PizzaName.Margherita, PizzaSize.Medium, true, clientLocation);
+            Order order = client.CreateOrder(client.Name, PizzaName.Margherita, PizzaSize.Medium, false, clientLocation);
             var deliveryman = new Deliveryman { Client = client };
             var shopAssistant = new ShopAssistant("Igor") { Deliveryman = deliveryman };
             shopAssistant.AcceptOrder(order);
-
-            shopAssistant.AcceptOrder(order);
-
         } 
     }
 }
